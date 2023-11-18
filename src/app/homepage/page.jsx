db4adc217c6da4de2179c86b2a52c1db1e1ea4ea
Ellams George <ellamsgeorge7@gmail.com>
2023-11-18 19:08:01 +0100

    landing page styling

--- src/app/homepage/page.jsx
+++ src/app/homepage/page.jsx
@@ -1,21 +1,32 @@
 const Homepage = () => {
-    return (
-    
+  return (
     <div className="">
 
-<div className="flex">
+      <div className="flex">
+        <div className="w-[50%] flex justify-center  bg-[white]">
+          <img className="lg:h-[610px]  " src="/images/interlude.png" />
+        </div>
+        <div className="w-[50%] border-l-2 border-black flex justify-center bg-[#e9e7e7] items-center ">
+          <img className="lg:h-[410px]" src="/images/all.png" />
+        </div>
+      </div>
+<div className="font-semibold">
 
-    <div className="w-[50%] flex justify-center  bg-white">
-    <img className="h-[610px]  " src="/images/interlude.png"/>
+<div>
+    <p>FREQUENTLY ASKED QUESTION</p>
+</div>
+
+<div className="flex justify-between border-t-2 border-black">
+    <div>What are your shipping costs?</div>
+    <div className="border-black border-l-2">What are your shipping costs?</div>
+    <div className="border-black border-l-2">What are your shipping costs?</div>
+</div>
+
+
+</div>
+
     </div>
-<div className="w-[50%]">
-<img className="w-full" src="/images/dark-glass.webp"/>
-</div>
+  );
+};
 
-
-</div>
-
-    </div>  );
-}
- 
 export default Homepage;
