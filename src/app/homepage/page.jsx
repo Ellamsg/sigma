3dd781926b9b0c1ad8b401b009c0de1e2270df36
Ellams George <ellamsgeorge7@gmail.com>
2023-11-21 19:28:56 +0100

    layouts

--- src/app/homepage/page.jsx
+++ src/app/homepage/page.jsx
@@ -14,7 +14,6 @@ const Homepage = () => {
           </div>
         </div>
 
-
         <div className="w-[50%] border-l-2 relative border-black flex justify-center bg-[#e9e7e7] items-center ">
           <img className="lg:h-[410px]" src="/images/all.png" />
           <div className="absolute bottom-2 left-4 bg-red-700">
@@ -22,8 +21,8 @@ const Homepage = () => {
             <p className="text-[22px] font-bold"> ORIGIN COLLECTION</p>
             <button className="text-[12px] banner-btn">DISCOVER NOW</button>
           </div>
-       
         </div>
+
       </div>
       <div className="font-semibold ">
         <div className="pl-4 py-3">
@@ -32,31 +31,31 @@ const Homepage = () => {
 
         <div className="flex lg:flex-row lg:h-[150px] w-full flex-col  border-t-2 border-black">
           <div className="border-black w-[100%] border-r-2 py-3  pl-4 ">
-            
             <p>What are your shipping costs?</p>
-            
-            </div>
-            <div className="border-black border-r-2 w-[100%] py-3 pl-4">
-            
+          </div>
+          <div className="border-black border-r-2 w-[100%] py-3 pl-4">
             <p>What are your shipping costs?</p>
-            
-            </div>
-            <div className="border-black w-[100%] border-r-2 py-3 pl-4">
-            
+          </div>
+          <div className="border-black w-[100%] border-r-2 py-3 pl-4">
             <p>What are your shipping costs?</p>
-            
-            </div>
+          </div>
         </div>
       </div>
-      
-      <div className="">
-        <img className="w-full" src="/images/woman-coat.png"/>
+
+    
+
+      <div className="relative">
+        <img className="w-full" src="/images/woman-coat.png" />
+        <div className=" absolute  flex justify-center left-0 right-0 items-center text-center  m-aut0 top-0 bottom-0 ">
+        <p className="text-6xl font-bold text-white ">KING SIGMA</p>
+        </div>
+     
+
       </div>
-      <Ads/>
+      <Ads />
       <div className="text-center p-3 border-t-2 border-b-2 border-black font-semibold">
         <p>BACK TO TOP</p>
       </div>
-
     </div>
   );
 };
--- src/components/footer/page.jsx
+++ src/components/footer/page.jsx
@@ -1,12 +1,56 @@
 const Footer = () => {
-    return ( 
-    
-    <div>
+  return (
+    <div className="flex">
+      <div className="flex p-6 w-[100%] lg:flex-row justify-between flex-col bg-red">
+        <div className="">
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+        </div>
 
-This is footer
+        <div>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+        </div>
+        <div>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+        </div>
+        <div>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+          <p>Terms of use</p>
+        </div>
+      </div>
 
 
-    </div> );
-}
- 
+      <div className="w-[100%] p-6">
+        <div>
+        <p className="font-bold">SIGN UP AND GET 10% OFF</p>
+    <p className="py-4">stay connected on time, subscribe to our newsletter and receive q0% off on your next order</p>
+
+        </div>
+<div className="flex flex-col gap-3">
+    <input className=" border-2 h-6 border-black"     type="text"/>
+    <button className="subscribe"> SUBSCRIBE</button>
+</div>
+      </div>
+    </div>
+  );
+};
+
 export default Footer;
